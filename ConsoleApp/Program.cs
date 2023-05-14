55fddfd142b77d8b5012aa10e8fc149cbb5db427
freddie1 <freddie1@keemail.me>
2023-05-14 12:45:00 +0300

    add sample console app

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using ConsoleApp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration(
+                    (context, configurationBuilder) =>
+                    {
+                        configurationBuilder.SetBasePath(args[0])
+                             .AddJsonFile("appsettings.json", false);
+                    })
+    .ConfigureServices((context, services) =>
+    {
+        
+        services.AddSingleton<ServiceA>();
+    })
+    .Build();
+
+
+var serviceA = host.Services.GetRequiredService<ServiceA>();
+serviceA.DoSomething();
