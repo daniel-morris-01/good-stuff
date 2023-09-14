4f63499d5db65d6399b6b40f3ffac60d314b30b3
freddie1 <freddie1@keemail.me>
2023-09-14 10:49:31 +0300

    py

---
+++ tantancoce/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace tantancoce 
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            double fd = 23.98;
+            int rt = Convert.ToInt32(fd);
+
+            Console.WriteLine(rt);
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
